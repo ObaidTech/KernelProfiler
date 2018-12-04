f2e2cd2d19f2d38e7ec9c41d5f79b48947136b76
root <mobaidullah@ryerson.ca>
2018-12-04 08:17:37 -0500

    adding other files

---
+++ cmake-build-debug/CMakeFiles/KernelProfiler.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/KernelProfiler.dir/main.cpp.o"
+  "KernelProfiler.pdb"
+  "KernelProfiler"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/KernelProfiler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
